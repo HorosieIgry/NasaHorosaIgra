2b743ba8a3ca021c0f4b4546bc08695aa3aec898
diabloso <diablo1994@poczta.onet.pl>
2015-04-21 18:00:08 +0200

    Update

--- Conspiracy/code/mechanics/Platforms.hpp
+++ Conspiracy/code/mechanics/Platforms.hpp
@@ -30,7 +30,7 @@ for(int i =0;i<20;i++){
 	if(f.floor[i]==1){
 			std::cout<<"Start"<<std::endl;
 		ziutek[i].setTexture(good_one(width,height));
-		ziutek[i].setColor(sf::Color(0,0,255));	
+		ziutek[i].setColor(sf::Color(0,255,255));	
 		ziutek[i].setPosition(i*width,SCRHEIGHT-height);
 		platforms.push_back(ziutek[i]);
 		std::cout<<"End"<<std::endl;
